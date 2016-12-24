05001aa31b7dbae13ce0f5f050f267a0ce052959
Dru Lang <drulang@gmail.com>
2016-12-24 15:56:12 -0500

    Implement the daily forecast request

--- Vain/OpenWeatherMapService.swift
+++ Vain/OpenWeatherMapService.swift
@@ -9,32 +9,45 @@
 import Foundation
 import Alamofire
 
-enum Router : URLRequestConvertible {
-    case currentForecast(location:Location)
-    
-    struct Config {
-        static let BaseURLString = "http://api.openweathermap.org/data/2.5"
-        static let Key = "36ddc8780ce4c36953b60d7c8e2d70d6" //TODO: Move into plist
-        static let KeyParameter = "appid"
-    }
-    
+fileprivate struct APIConfig {
+    static let BaseURLString = "http://api.openweathermap.org/data/2.5"
+    static let Key = "36ddc8780ce4c36953b60d7c8e2d70d6" //TODO: Move into plist
+    static let KeyParameter = "appid"
+}
 
+fileprivate struct APIParameters {
+    static let Location = "q"
+    static let Count = "cnt"
+}
+
+fileprivate enum Router : URLRequestConvertible {
+    case currentForecast(location:Location)
+    case dailyForecast(location:Location, numberOfDays:UInt)
+    
     func asURLRequest() throws -> URLRequest {
         let result: (path: String, parameters: Parameters) = {
             switch self {
             case .currentForecast(_):
                 let parameters = [
-                    "q" : "London"
+                    APIParameters.Location : "London"
                 ]
                 return ("/weather", parameters)
+                
+            case let .dailyForecast(_, numberOfDays):
+                let parameters = [
+                    APIParameters.Location: "London",
+                    APIParameters.Count: numberOfDays
+                    ] as [String : Any]
+                
+                return ("/forecast/daily", parameters)
             }
         }()
         
-        let url = try Config.BaseURLString.asURL()
+        let url = try APIConfig.BaseURLString.asURL()
         let urlRequest = URLRequest(url: url.appendingPathComponent(result.path))
         
         var urlParameters = result.parameters
-        urlParameters.updateValue(Config.Key, forKey: Config.KeyParameter)
+        urlParameters.updateValue(APIConfig.Key, forKey: APIConfig.KeyParameter)
 
         return try URLEncoding.default.encode(urlRequest, with: urlParameters)
     }
@@ -48,8 +61,12 @@ class OpenWeatherMapService {
 //MARK: WeatherDataSource
 extension OpenWeatherMapService: WeatherServiceDataSource {
     
+    // TODO: Change this to a daily forecast that takes the number of days as a param
     internal func fiveDayForecast(atLocation location: Location, completion: (MultiDayForecast?, WeatherServiceError?) -> Void) {
         
+        Alamofire.request(Router.dailyForecast(location: Location(), numberOfDays: 5)).responseJSON { (response) in
+            
+        }
 
         let forecast = MultiDayForecast()
         let f1 = Forecast()
